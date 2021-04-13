9a172a1e19806b07f7d02876aa391edb80a16dfa
Darina Ilieva <darina.ilieva@gmail.com>
2021-04-13 17:13:06 +0300

    JPAConfig and AuditConfig added.

---
+++ src/main/java/ea/ea/configurations/AuditConfig.java
@@ -0,0 +1,22 @@
+package ea.ea.configurations;
+
+import ea.ea.services.AuditorAwareImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * Configuration of auditing necessary for date of creation.
+ */
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class AuditConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
+}
+
---
+++ src/main/java/ea/ea/configurations/JPAConfig.java
@@ -0,0 +1,65 @@
+package ea.ea.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * Configuration of JPA.
+ */
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
+public class JPAConfig {
+
+    private String dbURl, dbUsername, dBPassword;
+
+    @Autowired
+    public JPAConfig(Environment env) {
+        dbURl = env.getProperty("database.url");
+        dbUsername = env.getProperty("database.username");
+        dBPassword = env.getProperty("database.password");
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(dbURl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dBPassword);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em
+                = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("ea.ea");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+
+        return em;
+    }
+
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+
+        return properties;
+    }
+}
